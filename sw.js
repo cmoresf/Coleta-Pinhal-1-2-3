e690ecacb9b553a1862b523546fdf2f9843146ba
cmoresf <cmoresf@gmail.com>
2026-08-19 12:16:12 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const C = "pinhal-v2";
+const ASSETS = ["./", "./index.html", "./icon.png"];
+self.addEventListener("install", e => { self.skipWaiting();
+  e.waitUntil(caches.open(C).then(c => c.addAll(ASSETS))); });
+self.addEventListener("activate", e => e.waitUntil(
+  caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k)))).then(() => self.clients.claim())));
+self.addEventListener("fetch", e => {
+  const req = e.request;
+  // rede primeiro (atualiza quando online); cai para cache quando offline
+  e.respondWith(
+    fetch(req).then(resp => {
+      const cp = resp.clone(); caches.open(C).then(c => c.put(req, cp)); return resp;
+    }).catch(() => caches.match(req).then(r => r || caches.match("./index.html")))
+  );
+});
